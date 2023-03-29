e2698d7d2d663a5522ee1c87b4807c432035cccf
RahulChatterjee1999 <112849417+RahulChatterjee1999@users.noreply.github.com>
2023-03-29 21:57:38 +0530

    fixd isauthenticated2

--- Routes/Other.js
+++ Routes/Other.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { Contactus } from "../Controllers/Other.js";
-import { isAuthenticated } from "../Middleware/Auth.js";
 const router=express.Router();
 
 router.route("/contact").post(Contactus);
